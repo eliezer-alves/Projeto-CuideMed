139869780e86e2d018b1c396151beca3eb08a482
Eliezer Alves <eliezer.cardoso@venturus.org.br>
2025-12-01 21:57:08 -0300

    feat: add swaggar api documentation

--- medicacao_hospitalar/urls.py
+++ medicacao_hospitalar/urls.py
@@ -17,7 +17,32 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+    SpectacularRedocView,
+)
+
 urlpatterns = [
     path("admin/", admin.site.urls),
+
+    # schema OpenAPI (JSON)
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+
+    # Swagger UI
+    path(
+        "api/docs/swagger/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+
+    # Redoc
+    path(
+        "api/docs/redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc",
+    ),
+
+    # app principal
     path("", include("core.urls")),
 ]
